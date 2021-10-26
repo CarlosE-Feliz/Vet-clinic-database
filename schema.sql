d326210fd9f71ec41f17b8cafb5ae4a320f41f0a
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-10-26 12:15:58 -0600

    query and update animals table

--- schema.sql
+++ schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE animals (
 	date_of_birth date NOT NULL,
 	escape_attempts integer NOT NULL,
 	neutered boolean NOT NULL,
-	weight_kg integer NOT NULL
+	weight_kg integer NOT NULL,
+	species VARCHAR(30)
 );
